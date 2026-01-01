5bfb1806920f00e73ab6ecc4d3d7721c93b5bf8a
vrabbi <scott@terasky.com>
2026-01-01 18:40:32 +0200

    Update backstage to 1.46.2

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,42 +1,10 @@
 import { createApp } from '@backstage/frontend-defaults';
 import { navModule } from './modules/nav';
 import { rootRedirectModule } from './modules/rootRedirect';
-import { createFrontendModule, githubAuthApiRef, gitlabAuthApiRef, microsoftAuthApiRef, SignInPageBlueprint } from '@backstage/frontend-plugin-api';
-import { SignInPage } from '@backstage/core-components';
+import { createFrontendModule } from '@backstage/frontend-plugin-api';
 import { teraskyThemeLight, teraskyThemeDark } from './theme';
 import { rbacPlugin, RbacPage } from '@backstage-community/plugin-rbac'
 import { convertLegacyPlugin, convertLegacyPageExtension } from '@backstage/core-compat-api';
-const signInPage = SignInPageBlueprint.make({
-  params: {
-    loader: async () => props =>
-      (
-        <SignInPage
-          {...props}
-          providers={[
-            'guest',
-            {
-              id: 'github-auth-provider',
-              title: 'GitHub',
-              message: 'Sign in using GitHub',
-              apiRef: githubAuthApiRef,
-            },
-            {
-              id: 'microsoft-auth-provider',
-              title: 'Microsoft',
-              message: 'Sign in using EntraID',
-              apiRef: microsoftAuthApiRef,
-            },
-            {
-              id: 'gitlab-auth-provider',
-              title: 'GitLab',
-              message: 'Sign in using GitLab',
-              apiRef: gitlabAuthApiRef,
-            },
-          ]}
-        />
-      ),
-  },
-});
 
 
 const convertedRbacAdminPage = convertLegacyPageExtension(RbacPage, {
@@ -52,7 +20,7 @@ export default createApp({
   features: [
     createFrontendModule({
       pluginId: 'app',
-      extensions: [signInPage, teraskyThemeLight, teraskyThemeDark],
+      extensions: [teraskyThemeLight, teraskyThemeDark],
     }),
     rootRedirectModule,
     navModule,
