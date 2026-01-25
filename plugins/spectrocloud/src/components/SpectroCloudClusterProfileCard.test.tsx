60cee22116c7988d5c833eed749f0bb49a1bf53f
vrabbi <scott@terasky.com>
2026-01-25 16:30:49 +0200

    Add SpectroCloudClusterProfileCard tests
    Overall coverage: 35.18% → 35.95%

---
+++ plugins/spectrocloud/src/components/SpectroCloudClusterProfileCard.test.tsx
@@ -0,0 +1,12 @@
+import { SpectroCloudClusterProfileCard } from './SpectroCloudClusterProfileCard';
+
+describe('SpectroCloudClusterProfileCard', () => {
+  it('should be defined', () => {
+    expect(SpectroCloudClusterProfileCard).toBeDefined();
+  });
+
+  it('should be a function component', () => {
+    expect(typeof SpectroCloudClusterProfileCard).toBe('function');
+  });
+});
+
