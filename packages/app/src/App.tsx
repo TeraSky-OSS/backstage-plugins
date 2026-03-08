f77046a6cf0e427c1962aeb70fd5741159857be6
vrabbi <scott@terasky.com>
2026-03-08 13:03:33 +0200

    Remove converted RBAC plugin and add new version with direct NFS support

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -3,18 +3,6 @@ import { navModule } from './modules/nav';
 import { rootRedirectModule } from './modules/rootRedirect';
 import { createFrontendModule } from '@backstage/frontend-plugin-api';
 import { teraskyThemeLight, teraskyThemeDark } from './theme';
-import { rbacPlugin, RbacPage } from '@backstage-community/plugin-rbac'
-import { convertLegacyPlugin, convertLegacyPageExtension } from '@backstage/core-compat-api';
-
-
-const convertedRbacAdminPage = convertLegacyPageExtension(RbacPage, {
-  name: 'rbac',
-  path: '/rbac',
-});
-
-const convertedRbacPlugin = convertLegacyPlugin(rbacPlugin, {
-  extensions: [convertedRbacAdminPage],
-});
 
 export default createApp({
   features: [
@@ -24,6 +12,5 @@ export default createApp({
     }),
     rootRedirectModule,
     navModule,
-    convertedRbacPlugin,
   ],
 });
