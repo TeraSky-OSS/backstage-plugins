197a583ccfda1547ad412fd1bd7daddec92611b5
Erik Miller <erik.miller@gusto.com>
2026-04-19 09:24:27 -0700

    feat(kubernetes-ingestor): re-export DefaultKubernetesResourceFetcher from public entry
    Signed-off-by: Erik Miller <erik.miller@gusto.com>

--- plugins/kubernetes-ingestor/src/index.ts
+++ plugins/kubernetes-ingestor/src/index.ts
@@ -1,4 +1,5 @@
 export { catalogModuleKubernetesIngestor as default } from './module';
 export { KubernetesEntityProvider, XRDTemplateEntityProvider } from './providers';
 export type { DeltaEvent } from './providers';
+export { DefaultKubernetesResourceFetcher } from './services';
 export type { KubernetesResourceFetcher, KubernetesResourceFetcherOptions } from './types';
